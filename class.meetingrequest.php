956f73f45d067a795e2e05b41179ca5aa4d47816
Mike Williams <mike.williams@grommunio.com>
2025-10-22 08:41:29 +0200

    php: fix updatecounter increment logic in updateMeetingRequest

--- class.meetingrequest.php
+++ class.meetingrequest.php
@@ -1524,7 +1524,7 @@ class Meetingrequest {
 			$this->setMeetingRequest($basedate);
 		}
 		else {
-			$counter = (isset($messageprops[$this->proptags['last_updatecounter']]) ?? 0) + 1;
+			$counter = ($messageprops[$this->proptags['last_updatecounter']] ?? 0) + 1;
 
 			// increment value of last_updatecounter, last_updatecounter will be common for recurring series
 			// so even if you sending an exception only you need to update the last_updatecounter in the recurring series message
